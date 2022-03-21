22fde685206e28488525db8edfef05612c2faac1
Giuxpro <albertogiusepp@gmail.com>
2022-03-21 04:39:06 -0300

    codigo env

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,13 +1,15 @@
-const API = "https://api.themoviedb.org/3";
+
+const API = process.env.REACT_APP_API;
+const API_TOKEN = process.env.REACT_APP_API_TOKEN;
 
 
 export function get(path){
 
  return fetch(API + path, {
          headers:{
-            Authorization:"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYzFmZDVhOTYwNDE5NjZlMTVlMDE3Y2EyOGRkOTQ2OCIsInN1YiI6IjYyMzY1NWM4MWRhN2E2MDA0NzcxYmU0OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zOm2SiGhLAWBPwxzw-fc6MEaw977E-xyJQSGKtUWG5w",
+            Authorization:"Bearer " + API_TOKEN,
 
             "Content-Type": "application/json;charset=utf-8",
         },
-    }).then(res => res.json())
+    }).then(res => res.json());
 }
